be22d792acdf85444043bfa84eb42e4f12a1f04e
Exokem <ex010@live.com>
2021-08-01 18:29:05 -0400

    Setup basic importing for task data on client load. Improvements are needed for indexing to ensure ordering and identifier consistency.

--- content/js/transponder.js
+++ content/js/transponder.js
@@ -1,6 +1,30 @@
 
 const URL = `transponder`
 
+const GET_JSON = { method: `GET`, headers: { 'Content-type': 'application/json'} }
+
+const TASK_DATA = fetch(`/data/task`, GET_JSON)
+    .then(function(response) { return response.json() })
+    .then((data) => 
+    {
+        data.files.forEach(file =>
+        {
+            console.log(`Emitting request for file '${file}'`)
+
+            fetch(file, GET_JSON)
+            .then(function(response) { return response.json() })  
+            .then(data => 
+            {
+                var task = Task.importJson(data)
+                document.getElementById('task-view').appendChild(task.displayMargin())
+            })    
+            .catch((err) => 
+            { 
+                console.log(`Request failed for '${file}'`); console.log(err) 
+            }) 
+        })    
+    })
+
 function tsExport(data)
 {
     fetch(URL, 
--- exodus.js
+++ exodus.js
@@ -10,12 +10,17 @@ const path = require('path')
 const hostname = '127.0.0.1'
 const port = 8977
 
+const JSON_OUT = `/data/task/`
+
 // Use parser middleware
 exodus.use(express.urlencoded({ extended: false })) 
 exodus.use(express.json())
 
+exodus.use(express.static(`data`))
+exodus.use(`/task`, express.static(__dirname + `data/task`))
+
 // Include content directories
-exodus.use(express.static('content'))
+exodus.use(express.static(`content`))
 exodus.use('/css', express.static(__dirname + 'content/css'))
 exodus.use('/html', express.static(__dirname + 'content/html'))
 exodus.use('/js', express.static(__dirname + 'content/js'))
@@ -34,20 +39,63 @@ exodus.post(`/transponder`, (request, response) =>
     writeJson(request.body)
 })
 
+var index = { files: [] }
+
+refreshIndices()
+
+exodus.get(JSON_OUT, (request, response) =>
+{
+    console.log(`Received request emission for '${JSON_OUT}'`)
+    response.json(index)  
+})
+
 exodus.listen(port, hostname, () => 
 {
     console.log(`Listening on http://${hostname}:${port}/`)
 })
 
-const JSON_OUT = `data/task/`
-
-function writeJson(json)
+async function refreshIndices()
 {
-    var path = `${JSON_OUT}task-${json.identifier}-${json.title}.vxtk`
+    fs.readdir(__dirname + JSON_OUT, (err, files) =>
+    {
+        if (err) { console.log(err); return }
+
+        
+
+        files.forEach(file => 
+        {
+            if (!index.files.includes(file))
+            {
+                index.files.push(file)
+            
+                exodus.get('/' + file, (request, response) =>
+                {
+                    console.log(`Received request emission for '${file}'`)
+
+                    fs.readFile(__dirname + JSON_OUT + file, (err, content) =>
+                    {
+                        var data = JSON.parse(content)
+                        
+                        console.log(`Returning json data '${data} for '${file}''`)
+                        response.json(data)
+                    })
+                })
+
+                console.log(`Established receiver for '${file}'`)
+            }
+        }) 
+    })
+}
+
+const WJSON_OUT = `data/task/`
+
+async function writeJson(json)
+{
+    var path = `${WJSON_OUT}${json.title}-${json.identifier}.vxtk`
     console.log(`Writing task data to '${path}'`)
     
-    // fs.writeFile(path, JSON.stringify(json), (err) =>
-    // {
+    fs.writeFile(path, JSON.stringify(json), (err) =>
+    {
 
-    // })
+    })
 }
